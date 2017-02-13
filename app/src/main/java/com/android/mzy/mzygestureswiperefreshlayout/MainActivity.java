2f35896d2b2ef3673a6ed71a2f5ad86dbc491860
mingzhangyong <504880928@qq.com>
2017-02-13 11:37:23 +0800

    fix minSdkVersion

---
+++ app/src/main/java/com/android/mzy/mzygestureswiperefreshlayout/MainActivity.java
@@ -0,0 +1,15 @@
+package com.android.mzy.mzygestureswiperefreshlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.mzy.gestureswiperefreshlayout.GestureSwipeRefreshLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
